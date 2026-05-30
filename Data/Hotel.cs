e348175bc0245a6d4da1ffbb307edb6dc359638f
Elvin Dzhavadov <eljav04@mail.ru>
2026-05-30 11:25:14 +0400

    Added Country entity, Added dbContext

--- Data/Hotel.cs
+++ Data/Hotel.cs
@@ -6,4 +6,7 @@ public class Hotel
     public required string Name { get; set; }
     public required string Address { get; set; }
     public double Rating { get; set; }
+    
+    public int CountryId { get; set; }
+    public Country? Country { get; set; }
 }
